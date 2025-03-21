0192f6938b53c1bca73b8ce90c1b45abcae8601a
yuval <yuvala2001@gmail.com>
2025-03-21 10:01:59 +0200

    changed ssd recommended to required

--- queries/games/add_game.py
+++ queries/games/add_game.py
@@ -5,7 +5,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 client = AsyncIOMotorClient('mongodb://localhost:27017')
 db = client["game_db"]
 collection = db.games
-
+#TODO add OS supported
 
 # Add new game
 async def add_one_game(name,
@@ -14,7 +14,7 @@ async def add_one_game(name,
                        release_date,
                        genres,
                        desc,
-                       is_ssd_recommended,
+                       is_ssd_required,
                        upscale_support,
                        api_support,
                        trailer_url,
@@ -30,7 +30,7 @@ async def add_one_game(name,
     :param release_date: Release date of the game
     :param genres: array of strings of genres
     :param desc: description of the game
-    :param is_ssd_recommended: True if the player should install the game on an SSD. otherwise, false.
+    :param is_ssd_required: True if the player should install the game on an SSD. otherwise, false.
     :param upscale_support: array of upscalers supported by the game. (E.G DLSS)
     :param api_support: array of APIs supported by the game. (E.G DX12 and Vulkan)
     :param trailer_url: URL of the trailer in YouTube
@@ -47,7 +47,7 @@ async def add_one_game(name,
         "release_date": release_date,
         "genres": genres,
         "desc": desc,
-        "isSsdRecommended": is_ssd_recommended,
+        "isSsdRequired": is_ssd_required,
         "upscaleSupport": upscale_support,
         "apiSupport": api_support,
         "trailer_url": trailer_url,
@@ -59,29 +59,31 @@ async def add_one_game(name,
 
 
 async def main():
-    name = "God of War Ragnarok"
+    name = "Kingdom Come Deliverance 1"
 
-    publisher = "Sony Interactive Entertainment"
+    publisher = "Deep Silver"
 
-    developer = "Santa Monica Studio"
+    developer = "Warhorse Studios"
 
-    release_date = 2022
+    release_date = 2018
 
     genres = ["Action", "Adventure", "RPG"]
 
-    desc = "Kratos and Atreus embark on a mythic journey for answers before Ragnarök arrives "
+    desc = ("Story-driven open-world RPG that immerses you in an epic adventure in the Holy Roman Empire. Avenge "
+            "your parents' death as you battle invading forces, go on game-changing quests, and make influential "
+            "choices. Explore castles, forests, villages and other realistic settings in medieval Bohemia!")
 
-    is_ssd_recommended = True
+    is_ssd_required = False
 
-    upscale_support = ["NVIDIA DLSS 3.7", "AMD FSR 3.1", "Intel XeSS 1.3"]
+    upscale_support = []
 
-    api_support = ["DX12"]
+    api_support = ["DX11"]
 
-    trailer = "https://www.youtube.com/watch?v=EE-4GvjKcfs"
+    trailer = "https://www.youtube.com/watch?v=_D48PCFshHg"
 
-    portrait = "https://imgur.com/CyHn5T3"
+    portrait = "https://imgur.com/q1aVXnw"
 
-    landscape = "https://imgur.com/Ds1R60w"
+    landscape = "https://imgur.com/DgbCcxa"
 
     buy_links = []
 
@@ -91,7 +93,7 @@ async def main():
                        release_date,
                        genres,
                        desc,
-                       is_ssd_recommended,
+                       is_ssd_required,
                        upscale_support,
                        api_support,
                        trailer,
