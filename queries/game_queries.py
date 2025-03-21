6de7b694110957b111c435a1867380f828d1a51b
yuval <yuvala2001@gmail.com>
2025-03-21 10:03:18 +0200

    removed redundant code

--- queries/game_queries.py
+++ queries/game_queries.py
@@ -13,34 +13,6 @@ db = client['game_db']
 games_collection = db.get_collection('games')
 
 
-# Add new game
-def create_game(name, publisher, release_date, portrait_url, landscape_url, additional_info=None):
-    """
-    Adds a new game to the database.
-    :param name: Name of the game
-    :param publisher: Publisher of the game
-    :param release_date: Release date of the game
-    :param portrait_url: URL for the portrait image
-    :param landscape_url: URL for the landscape image
-    :param additional_info: Any other relevant info (optional)
-    """
-    game_id = name.lower().replace(' ', '_') + "_" + str(release_date)
-    new_game = {
-        "id": game_id,
-        "name": name,
-        "publisher": publisher,
-        "release_date": release_date,
-        "portrait_url": portrait_url,
-        "landscape_url": landscape_url,
-        "requirements": {},  # Initialize the requirements as an empty dictionary
-    }
-    if additional_info:
-        new_game["additional_info"] = additional_info
-
-    db.games.insert_one(new_game)
-    print(f"Game '{name}' added to the database.")
-
-
 async def get_game(game_id: str):
     """
     Gets a game from the database using MongoDB's built-in ids.
