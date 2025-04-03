9fd98b2ff830a5159b602002e4c24afe377ada18
yuval <yuvala2001@gmail.com>
2025-04-03 11:48:18 +0300

    Moved validation of games to validation.py file

--- backend/utils/validation.py
+++ backend/utils/validation.py
@@ -1,5 +1,5 @@
 import re
-from typing import Optional
+from typing import Optional, List
 from fastapi import HTTPException
 
 
@@ -22,7 +22,7 @@ def validate_hardware_list(
        :param brand: Optional brand filter to validate against.
        :param model: Optional model regex to validate against.
        """
-# If hardware list is empty count it as no CPU/GPU found error
+    # If hardware list is empty count it as no CPU/GPU found error
     if not hardware:
         raise HTTPException(status_code=404, detail=f"No {type_} found")
     # Ensure only relevant CPUs have been fetched from DB
@@ -41,3 +41,60 @@ def validate_hardware_list(
                     model_pattern.search(item.get("fullname", ""))
             ):
                 raise HTTPException(status_code=500, detail=f"Wrong model regex found in {type_}s fetched")
+
+
+def validate_games_list(games: list,
+                        limit: Optional[int] = None,
+                        name: Optional[str] = None,
+                        publisher: Optional[str] = None,
+                        developer: Optional[str] = None,
+                        release_date: Optional[int] = None,
+                        genre: Optional[str] = None
+                        ):
+    """
+       Validates the list of games fetched from the DB.
+
+       - Raises 404 if the list is empty.
+       - Raises 500 if any item has invalid filtering (e.g. having wrong publisher or wrong release date)
+
+
+       :param games: List of games dictionaries from the DB.
+       :param limit: limited number of games that should be returned, if None - no limit.
+       :param name: desired name of a game
+       :param publisher: desired publisher of a game
+       :param developer: desired developer of a game
+       :param release_date: desired release year (int) of a game
+       :param genre: desired genre of a game
+       """
+    # If games list is empty count it as no games found error
+    if not games:
+        raise HTTPException(status_code=404, detail="No games found")
+    # Ensure the correct amount of games was returned
+    if limit is not None and len(games) > limit:
+        raise HTTPException(status_code=500, detail="Too many games found")
+
+    # Ensure only relevant games have been fetched from DB
+    for item in games:
+        # Ensure the name's regex matches the game's name (if used)
+        name_pattern = re.compile(name, re.IGNORECASE) if name else None
+        if name is not None and not name_pattern.search(item.get("name", "")):#
+            raise HTTPException(status_code=500, detail="Wrong name found in games route")
+
+        # Ensure the publisher's regex matches the game's publisher (if used)
+        publisher_pattern = re.compile(publisher, re.IGNORECASE) if publisher else None
+        if publisher is not None and not publisher_pattern.search(item.get("publisher", "")):#
+            raise HTTPException(status_code=500, detail="Wrong publisher found in games route")
+
+        # Ensure the developer's regex matches the game's developer
+        developer_pattern = re.compile(developer, re.IGNORECASE) if developer else None
+        if developer is not None and not developer_pattern.search(item.get("developer", "")):
+            raise HTTPException(status_code=500, detail="Wrong developer found in games route")
+
+        # Ensure the release date matches (years should be equal)
+        if release_date is not None and item.get("release_date", "") != release_date:
+            raise HTTPException(status_code=500, detail="Wrong release date found in games route")
+
+        # Ensure the genre's regex is in the game's genres list
+        genre_pattern = re.compile(genre, re.IGNORECASE) if genre else None
+        if genre is not None and not any(genre_pattern.search(g) for g in item.get("genres", [])):
+            raise HTTPException(status_code=500, detail="genre not found in game's genres in games route")
